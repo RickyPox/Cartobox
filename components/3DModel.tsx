dbf7d5ecf3012d346565ca362201ea95173d19f0
Ricky <ricardojsribeiro99@gmail.com>
2026-01-08 18:33:40 +0000

    Modelo 3D na HomePage

---
+++ components/3DModel.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, useGLTF, Center, PerspectiveCamera } from "@react-three/drei";
+import * as THREE from "three";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+function Model() {
+    const { scene } = useGLTF("/model/Cartobox.glb");
+
+    return (
+        <Center>
+            <primitive
+                object={scene}
+                scale={1}
+                position={[-0.2, 50, 0]} // ajusta altura do modelo
+            />
+        </Center>
+    );
+}
+
+// Componente para animar a câmera
+function CameraAnimation({
+    cameraRef,
+    setOrbitEnabled,
+    distance,
+}: {
+    cameraRef: React.RefObject<THREE.PerspectiveCamera | null>;
+    setOrbitEnabled: (value: boolean) => void;
+    distance: number;
+}) {
+    const initialDistanceRef = useRef(distance);
+    useEffect(() => {
+        if (!cameraRef.current) return;
+        const z = initialDistanceRef.current;
+        const camera = cameraRef.current;
+        const target = new THREE.Vector3(0, 0, 0);
+        // Desativa orbitControls no início
+        setOrbitEnabled(false);
+
+        // Animação da posição da câmera
+        gsap.fromTo(
+            camera.position,
+            { x: 500, y: 500, z: 500 },
+            {
+                x: 0,
+                y: 0,
+                z: z,
+                duration: 1.5,
+                ease: "power2.inOut",
+                onUpdate: () => {
+                    camera.lookAt(target); // mantém a rotação sincronizada
+                },
+                onComplete: () => {
+                    // Ativa orbitControls quando termina
+                    setOrbitEnabled(true);
+                },
+            }
+        );
+    }, [cameraRef, setOrbitEnabled]);
+
+    return null;
+}
+
+export default function ThreeModel() {
+    const [distance, setDistance] = useState(185); // default desktop
+    const [orbitEnabled, setOrbitEnabled] = useState(false);
+    const cameraRef = useRef<THREE.PerspectiveCamera>(null);
+
+    const degToRad = (deg: number) => (deg * Math.PI) / 180;
+
+    // Ajusta distância do OrbitControls conforme tamanho da tela
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+
+            if (width < 425) setDistance(450); // mobile menor
+            else if (width < 640) setDistance(300); // mobile maior
+            else if (width < 1024) setDistance(270); // tablet
+            else setDistance(185); // desktop
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <Canvas>
+            {/* CÂMERA */}
+            <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 10, 50]} fov={45} />
+
+            {/* ANIMAÇÃO DA CÂMERA */}
+            <CameraAnimation distance={distance} cameraRef={cameraRef} setOrbitEnabled={setOrbitEnabled} />
+
+            {/* LUZES */}
+            <ambientLight intensity={2} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+
+            {/* MODELO */}
+            <Suspense fallback={null}>
+                <Model />
+            </Suspense>
+
+            {/* ORBIT CONTROLS */}
+            <OrbitControls
+                enabled={orbitEnabled}
+                enableZoom={false}
+                enablePan={false}
+                minDistance={distance}
+                maxDistance={distance}
+                minAzimuthAngle={degToRad(-20)}
+                maxAzimuthAngle={degToRad(20)}
+                minPolarAngle={degToRad(60)}
+                maxPolarAngle={degToRad(100)}
+            />
+        </Canvas>
+    );
+}
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,11 +1,12 @@
 import Button from "../components/Button";
+import ThreeModel from "../components/3DModel";
 
 export default function Home() {
     return (
-        <section className="bg-secondary flex justify-center">
+        <section className="bg-secondary flex justify-center sm:pb-160 pb-140  relative overflow-x-hidden">
             <div className="container ">
-                <div className="wrapper flex-column gap-y-8">
-                    <div className=" flex-column gap-y-6">
+                <div className="wrapper flex-column gap-y-8 relative">
+                    <div className=" flex-column gap-y-6 max-w-5xl">
                         <h1>Embalagens à medida do seu negócio</h1>
                         <p>
                             Na Cartobox procuramos responder às necessidades individuais de cada empresa, personalizando a impressão, dimensão e
@@ -17,6 +18,9 @@ export default function Home() {
                         <Button text="Encontre a embalagem à sua medida" link="/contactos" bgColor="var(--primary-color)" />
                     </div>
                 </div>
+            </div>
+            <div className="h-200 w-full absolute xl:top-60 xl:-right-50 lg:top-90 top-60 ">
+                <ThreeModel />
             </div>
         </section>
     );
